64bc67e483b8193397cbd6924c8862bb5703f575
avikannan <65484560+avikannan@users.noreply.github.com>
2020-05-17 10:38:40 +0200

    Add files via upload
    This is the first assignment in the capstone project

---
+++ Capstone_Project.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Capstone project
+# This notebook contains python script for the final **capstone project** as part of the IBM Data Science Proffessional Course
+# 
+# 
+# This book will be part of my github repository
+# 
+
+# In[5]:
+
+
+import pandas as pd
+import Numpy as np
+
+
+# In[6]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
