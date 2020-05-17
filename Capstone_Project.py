a36b8bea37c74f022aca5b4b6fd3ab5f11f866b3
avikannan <65484560+avikannan@users.noreply.github.com>
2020-05-17 10:48:13 +0200

    Update Capstone_Project.py

--- Capstone_Project.py
+++ Capstone_Project.py
@@ -4,15 +4,12 @@
 # ## Capstone project
 # This notebook contains python script for the final **capstone project** as part of the IBM Data Science Proffessional Course
 # 
-# 
 # This book will be part of my github repository
-# 
-
 # In[5]:
 
 
 import pandas as pd
-import Numpy as np
+import numpy as np
 
 
 # In[6]:
